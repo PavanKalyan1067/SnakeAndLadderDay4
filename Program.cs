5364e747e75e894ac0ed761895892bd74a9ce139
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 09:56:03 +0530

    Used random to check for options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakeAndLadder
 {
@@ -6,12 +7,31 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake And Ladder Problem!");
             int playerPos = 0, rollDice;
+            //creates random number for rolling dice
             Random random = new Random();
             rollDice = random.Next(1, 7);
             Console.WriteLine("Rolled Dice number is:" + rollDice);
+            // print user option
+            Console.WriteLine("Player Option 1.No Play 2.Ladder 3.Snake");
+            var list = new List<string> { "No Play", "Ladder", "Snake" };
+            //check user option
+            int pos = random.Next(list.Count);
+            //condition based on user option
+            if (list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
 
+            }
+            else if (list[pos] == "Snake")
+            {
+                playerPos -= rollDice;
+
+            }
+
+            //print result of playerposition and dice rolled
+            Console.WriteLine("Player option:" + list[pos]);
+            Console.WriteLine("player updated position:" + playerPos);
         }
     }
 }
