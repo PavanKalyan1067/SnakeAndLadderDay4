db6ae2cb62f17d2ea34bd80f0ca8c162aa255ab3
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 09:53:23 +0530

    Roll dice to get a number using Random

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Problem!");
-
-            int playerPos = 0;
+            int playerPos = 0, rollDice;
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("Rolled Dice number is:" + rollDice);
 
         }
     }
