eb4918ee6e9eda3fe102665759f5e117ec573037
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 09:59:41 +0530

    Ensure player Wins

--- Program.cs
+++ Program.cs
@@ -7,28 +7,40 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+
             //local variable
             int rollDice, playerPos = 0;
+            int count = 0;
 
             //To generate random number
             Random random = new Random();
+
             //List to check options
             var list = new List<string> { "No Play", "Ladder", "Snake" };
-
             //Conditions for user options
             rollDice = random.Next(1, 7);
             Console.WriteLine("Dice number is : " + rollDice);
-
             //Print user options
             Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
 
             //Use random to check user option
-            int index = random.Next(list.Count);
-            Console.WriteLine("Player option is : " + list[index]);
+            int pos = random.Next(list.Count);
+            Console.WriteLine("Player option is : " + list[pos]);
             if (playerPos + rollDice < 100)
             {
-                if (list[index] == "Ladder") playerPos += rollDice;
-                if (list[index] == "Snake") playerPos -= rollDice;
+                if (list[pos] == "Ladder")
+                {
+                    playerPos += rollDice;
+                }
+                if (list[pos] == "Snake")
+                {
+                    playerPos -= rollDice;
+                }
+            }
+            else if (playerPos + rollDice == 100 && list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
+
             }
 
             if (playerPos < 0)
@@ -38,7 +50,10 @@ namespace SnakeAndLadder
             //Print to Console
             Console.WriteLine("\n");
             Console.WriteLine("Player current position : " + playerPos);
+            count++;
             Console.WriteLine("Final position is :" + playerPos);
+            Console.WriteLine("Total MOves:" + count);
         }
     }
+   
 }
